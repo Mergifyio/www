0fefbe64830a9d07e86b5f627cf40b033aad248b
Julien Danjou <julien@danjou.info>
2021-01-29 18:20:59 +0100

    fix(blog): move prism to Ghost

--- blog.mergify.io/script.js
+++ blog.mergify.io/script.js
@@ -10,15 +10,3 @@ ga('send', 'pageview');
 
 // Mailchimp
 !function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/339c31be17ff8e8e65b0e6466/7ef49deade42c6f4640cd8a30.js");
-
-// External deps
-function loadScript(url) {
-  var script = document.createElement("script");
-  script.src = url;
-  document.head.appendChild(script);
-}
-
-loadScript("https://cdnjs.cloudflare.com/ajax/libs/prism/1.20.0/prism.min.js");
-loadScript("https://cdnjs.cloudflare.com/ajax/libs/prism/1.20.0/components/prism-python.min.js");
-loadScript("https://cdnjs.cloudflare.com/ajax/libs/prism/1.20.0/components/prism-yaml.min.js");
-loadScript("https://cdnjs.cloudflare.com/ajax/libs/prism/1.20.0/components/prism-graphql.min.js");
