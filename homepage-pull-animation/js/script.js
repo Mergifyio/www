d974139203c3696357b62fc4ddb3d61ff6aadba0
Mehdi Abaakouk <sileht@sileht.net>
2021-04-22 10:19:17 +0200

    feat: add homepage-pull-animation

---
+++ homepage-pull-animation/js/script.js
@@ -0,0 +1,81 @@
+
+// ===============================================================
+// DOCUMENT READY INIT
+// ===============================================================
+
+let amItems = Array.from(document.querySelectorAll('.a-merg__item'));
+const amWrapper = document.querySelector('.a-merg__items-list');
+const amRobot = document.querySelector('.a-merg__robot');
+const amRobotHead = document.querySelector('.a-merg__robot__head');
+const amRobotLoading = document.querySelector('.a-merg__robot__loading');
+
+let amWaitingItem;
+
+window.addEventListener('load', function(){
+
+	setTimeout(function(){
+		amWaitingItem = amItems[amItems.length-1];
+		amAddItem(amWaitingItem);
+	},300);
+});
+
+
+function amAddItem(el){
+	el.classList.remove('a-merg__item--waiting');
+
+	setTimeout(function(){
+		amRobot.classList.add('a-merg__robot--surprised');
+
+		setTimeout(function(){
+			amRobot.classList.remove('a-merg__robot--surprised');
+			amScann();
+
+		},800);
+
+	},200);
+};
+
+function amScann(){
+
+	amRobot.classList.add('a-merg__robot--scanning');
+	amItems[0].classList.add('a-merg__item--scanning');
+
+	setTimeout(function(){
+		amRobot.classList.remove('a-merg__robot--scanning');
+		amItems[0].classList.remove('a-merg__item--scanning');
+		amValidate();
+		
+	},3000);
+
+};
+
+function amValidate(){
+	amItems[0].classList.add('a-merg__item--merged');
+	amWrapper.classList.add('next');
+
+	setTimeout(function(){
+		amRobot.classList.add('a-merg__robot--success');
+	},200);
+
+	setTimeout(function(){
+		amRobot.classList.remove('a-merg__robot--success');
+		amEnd(amItems[0]);
+	},800);
+};
+
+function amEnd(el){
+
+	setTimeout(function(){
+		el.classList.remove('a-merg__item--merged');
+		el.classList.add('a-merg__item--waiting');
+		amWrapper.classList.remove('next');
+
+		var dupNode = el.cloneNode(true);
+		el.remove();
+		amWrapper.appendChild(dupNode);
+		amItems = Array.from(document.querySelectorAll('.a-merg__item'));
+		setTimeout(function(){
+			amAddItem(amItems[amItems.length-1]);
+		},300);
+	},1000);
+};
