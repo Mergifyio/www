fb1a711f1509ecafa9da0ef9ec0cf7843bcedcfb
Julien Danjou <julien@danjou.info>
2023-05-03 16:51:06 +0200

    remove unused script

--- blog.mergify.com/script.js
+++
@@ -1,2 +0,0 @@
-// Mailchimp
-!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/339c31be17ff8e8e65b0e6466/7ef49deade42c6f4640cd8a30.js");
